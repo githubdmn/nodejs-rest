ae43bcb1f79b009c6bc01eda9e1dc778007890d7
githubdmn <dmninbox@gmail.com>
2024-05-27 19:07:09 +0200

    env refactored

--- src/conf/dev.env.ts
+++ src/conf/dev.env.ts
@@ -1,17 +1,44 @@
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+const requiredEnv = [
+  'DB_HOST',
+  'PORT',
+  'DATABASE',
+  'POSTGRES_USER',
+  'POSTGRES_PASSWORD',
+  'POSTGRES_PORT',
+  'POSTGRES_DB',
+  'POSTGRES_SCHEMA',
+  'POSTGRES_SYNC',
+  'POSTGRES_LOGGING',
+  'JWT_ACCESS',
+  'JWT_REFRESH',
+  'JWT_SECRET',
+];
+
+for (const envName of requiredEnv) {
+  if (!process.env[envName]) {
+    throw new Error(`Environment variable ${envName} is not defined`);
+  }
+}
+
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
+dotenv.config({ path: envFile });
 
 export default {
   environment: process.env.NODE_ENV,
   host: process.env.DB_HOST,
-  port: 3000,
+  port: parseInt(process.env.PORT ?? '3000'),
   dbUse: process.env.DATABASE,
   pguser: process.env.POSTGRES_USER,
   pgpassword: process.env.POSTGRES_PASSWORD,
   pgport: process.env.POSTGRES_PORT,
   pgdb: process.env.POSTGRES_DB,
-  jwtAccess: 'access',
-  jwtRefresh: 'refresh',
-  jwtSecret: 'secret',
+  pgType: process.env.DATABSE,
+  pgSchema: process.env.POSTGRES_SCHEMA,
+  pgSync: process.env.POSTGRES_SYNC,
+  pgLogging: process.env.POSTGRES_LOGGING,
+  jwtAccess: process.env.JWT_ACCESS,
+  jwtRefresh: process.env.JWT_REFRESH,
+  jwtSecret: process.env.JWT_SECRET,
 };
--- src/conf/postresql.connection.ts
+++ src/conf/postresql.connection.ts
@@ -1,49 +1,44 @@
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import env from './dev.env';
 
-env.pgport = typeof env.pgport === 'string' ? env.pgport : '';
-
-const settingEnv = {
-  type: 'postgres',
-  schema: 'public',
+const dbSettingEnv = {
+  type: env.pgType,
+  schema: env.pgSchema,
   database: env.pgdb,
   host: env.host,
-  port: parseInt(env.pgport),
+  port: env.pgport,
   username: env.pguser,
   password: env.pgpassword,
-  synchronize: true,
+  synchronize: env.pgSync,
   entities: ['dist/src/entities/*.entity.{ts,js}'],
-  logging: false,
+  logging: env.pgLogging,
 } as TypeOrmModuleOptions;
+
+export default TypeOrmModule.forRoot(dbSettingEnv);
 
 // in case the env fails mapping
-const settingBackup = {
-  type: 'postgres',
-  schema: 'public',
-  database: 'technical',
-  host: 'postgres',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  synchronize: true,
-  entities: ['dist/src/entities/*.entity.{ts,js}'],
-  logging: false,
-} as TypeOrmModuleOptions;
+// const settingBackup = {
+//   type: 'postgres',
+//   schema: 'public',
+//   database: 'technical',
+//   host: 'postgres',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'postgres',
+//   synchronize: true,
+//   entities: ['dist/src/entities/*.entity.{ts,js}'],
+//   logging: false,
+// } as TypeOrmModuleOptions;
 
-const settingLocal = {
-  type: 'postgres',
-  schema: 'public',
-  database: 'technical',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  synchronize: true,
-  entities: ['dist/src/entities/*.entity.{ts,js}'],
-  logging: false,
-} as TypeOrmModuleOptions;
-
-// environment can be 'production' od 'development'
-const setting = env.environment === 'production' ? settingEnv : settingLocal;
-
-export default TypeOrmModule.forRoot(setting);
+// const settingLocal = {
+//   type: 'postgres',
+//   schema: 'public',
+//   database: 'technical',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'postgres',
+//   synchronize: true,
+//   entities: ['dist/src/entities/*.entity.{ts,js}'],
+//   logging: false,
+// } as TypeOrmModuleOptions;
